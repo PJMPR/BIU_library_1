eb0711f11837a7bfe5b84623819028da4f56b7c8
klebek <s13928@pjwstk.edu.pl>
2017-10-03 10:05:26 +0200

    fx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     MainPageComponent,
-    RightMenuComponent
+    RightMenuComponent,
     HeaderComponent
   ],
   imports: [
