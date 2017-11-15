ebd88831e3e90449029cbf39f2a9036fa801004c
Lelental <p.lelental@gmail.com>
2017-11-15 19:37:00 +0100

    fix book service

--- src/app/booklist/bookModel.ts
+++ src/app/booklist/bookModel.ts
@@ -1,14 +1,16 @@
 export class BookModel {
 
     constructor(
+        public id:number,
         public imgURL:string,
         public title:string,
         public publisher:string,
         public authors:string[],
-        public quantity:number
+        public quantity:number        
     ) { }
 
-
+    
+    
    
 
 }
